5bf6556f68f60bbf1a7fb4a5c752b8cecf5d4ea3
Tony Garrett <tony.garrett@gmail.com>
2025-03-13 18:16:27 -0400

    Basic Wagtail skeleton set up

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-b&p)t=waf0(ok0rna1tt(4lw_i)v1_*0&xjj==jle(82*79*5d"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
